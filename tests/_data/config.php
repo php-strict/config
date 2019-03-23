a09a93f093d1a266e8c3f52a02baccd2b160b8f0
enikeishik <enikeishik@gmail.com>
2019-03-23 13:36:59 +0300

    feat: configs for tests

---
+++ tests/_data/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'debug' => true,
+    'debug_level' => 0,
+    'db_server' => 'localhost',
+    'db_user' => 'root',
+    'db_password' => '',
+    'db_name' => 'testproject',
+    'db_charset' => 'utf8',
+    'db_table_prefix' => '',
+    'db_readonly' => false,
+    'cache' => true,
+];
